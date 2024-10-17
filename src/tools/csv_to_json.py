ef402dfeea20d58f72286e0fed4b80987400fbeb
Ursusgulo <visu2055@student.uu.se>
2024-10-17 21:55:35 +0200

    tool: csv_to_json parser first version
    csv_to_json parser works but encoding is wrong

---
+++ src/tools/csv_to_json.py
@@ -0,0 +1,29 @@
+"""
+Program that converts csv file to json files.
+
+"""
+import csv
+import json
+
+def create_json(csvFilePath, jsonFilePath):
+    
+    data = {}
+    encoding = 'utf-8'
+    with open(csvFilePath, encoding = encoding) as csvf:
+        csvReader = csv.DictReader(csvf)
+        for rows in csvReader:
+            #grab JSON keys
+            key = rows['Program']
+            data[key] = rows
+            
+    with open(jsonFilePath, 'w', encoding = encoding) as jsonf:
+        jsonf.write(json.dumps(data, indent=4))
+
+
+# Decide the two file paths according to your 
+# computer system
+csvFilePath = r'Categorized_Master_Programs.csv'
+jsonFilePath = r'Names.json'
+
+# Call the make_json function
+create_json(csvFilePath, jsonFilePath)
