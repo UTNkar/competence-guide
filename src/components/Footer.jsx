debb641bd6ac4630a5fbb87b3e664db464ebb053
Freez1t <160607472+Freez1t@users.noreply.github.com>
2024-11-17 19:04:28 +0100

    Remake everything

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,123 +1,151 @@
-import { useState, useEffect, Fragment } from "react";
-
 import styles from "../css/FooterNavbarComponents/footer.module.css"
 
-// Custom components
-import FooterCol1 from "./FooterNavbarComponents/FooterCol1";
-import FooterCol2 from "./FooterNavbarComponents/FooterCol2";
-import FooterCol3 from "./FooterNavbarComponents/FooterCol3";
-import FooterCol4 from "./FooterNavbarComponents/FooterCol4";
-import FooterSocialsRow from "./FooterNavbarComponents/FooterSocialsRow";
+// Preferably MUI components should be used but this is stolen from utns website
 
-//MUI
-import { Box, Grid, Container, Button, Typography, Stack, Link } from "@mui/material";
-import GitHubIcon from "@mui/icons-material/GitHub";
-
-export function H3({children}) {
-  return(
-    <>
-      <h3 className={`footer_heading without_spacing`}>{children}</h3>
-      <div className={`h_after`}> </div>
-    </>
-      
-  )
+const FooterWrapper = ({children}) => {
+    return (
+        <section className={`${styles.footer}`}>
+            <div className={`${styles.w_layout_vflex} ${styles.container}`}>
+                <div className={`${styles.w_layout_vflex} ${styles.footer_wrapper}`}>
+                    {children}
+                </div>
+            </div>
+        </section>
+    )
 }
 
-export function H4({children}) {
-  return(
-    <>
-      <h4 className={`footer_subheading without_decoration`}>{children}</h4>
-    </>
-      
-  )
+
+const FooterLogo = () => {
+    return (
+        <div className={`${styles.w_layout_vflex} ${styles.footer_logo_wrapper}`}>
+            <a href="/" aria-current="page" className={`${styles.w_inline_block} ${styles.w__current}`}>
+                <img
+                src="https://cdn.prod.website-files.com/655e29844518537470ba5b0f/65bcf578eaa70a4540ea6e8a_utn_standard_bla%20(2).png"
+                loading="lazy" sizes="(max-width: 767px) 100vw, 187.296875px"
+                srcset="https://cdn.prod.website-files.com/655e29844518537470ba5b0f/65bcf578eaa70a4540ea6e8a_utn_standard_bla%20(2)-p-500.png 500w, https://cdn.prod.website-files.com/655e29844518537470ba5b0f/65bcf578eaa70a4540ea6e8a_utn_standard_bla%20(2)-p-800.png 800w, https://cdn.prod.website-files.com/655e29844518537470ba5b0f/65bcf578eaa70a4540ea6e8a_utn_standard_bla%20(2)-p-1080.png 1080w, https://cdn.prod.website-files.com/655e29844518537470ba5b0f/65bcf578eaa70a4540ea6e8a_utn_standard_bla%20(2)-p-1600.png 1600w, https://cdn.prod.website-files.com/655e29844518537470ba5b0f/65bcf578eaa70a4540ea6e8a_utn_standard_bla%20(2)-p-2000.png 2000w, https://cdn.prod.website-files.com/655e29844518537470ba5b0f/65bcf578eaa70a4540ea6e8a_utn_standard_bla%20(2).png 3227w"
+                alt="" className={`${styles.footer_logo}`}/>
+            </a>
+            <div className={`${styles.code_embed} ${styles.w_embed}`}>
+                <iframe
+                className={`${styles.footer_map}`}
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2004.5168622229132!2d17.64881977699934!3d59.84055606999524!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465fc95f989af5a9%3A0x2a2ee8953bd196b6!2sUppsala%20Teknolog-%20%26%20Naturvetark%C3%A5r!5e0!3m2!1ssv!2sse!4v1728127637716!5m2!1ssv!2sse"
+                width="280" height="280" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade">
+                </iframe>
+            </div>
+        </div>
+    )
+}
+
+const FooterInfo = ({children}) => {
+    return (
+        <div className={`${styles.w_layout_vflex} ${styles.footer_info_wrapper}`}>
+            {children}
+        </div>
+    )
+}
+
+const FooterHeading = ({children}) => {
+    return (
+        <h3 className={`${styles.without_spacing}`}>{children}</h3>
+    )
+}
+
+const FooterSubheading = ({children}) => {
+    return (
+        <h4 className={`${styles.footer_subheading}`}>{children}</h4>
+    )
+}
+
+const FooterLinks = ({children}) => {
+    return (
+        <p className={`${styles.standalone_links}`}>{children}</p>
+    )
+}
+
+const FooterCol = ({children}) => {
+    return (
+        <div className={`${styles.w_layout_vflex} ${styles.footer_column}`}>
+            {children}
+        </div>
+    )
+}
+
+const SocialLinksFooter = () => {
+    return (
+        <FooterCol>
+            <FooterHeading>Länkar</FooterHeading>
+            <FooterSubheading>Sociala medier</FooterSubheading>      
+            <FooterLinks>
+                <a href="https://facebook.com/utnkar">Facebook</a>
+                <br/>
+                <a href="https://instagram.com/utnkar">Instagram</a>
+                <br/>
+                <a href="https://linkedin.com/company/utnkar">LinkedIn</a>
+            </FooterLinks>  
+            <FooterLinks><br/></FooterLinks>  
+            <FooterSubheading>Språk</FooterSubheading>     
+            <div className={`${styles.spacer} ${styles.less}`}></div>   
+            <div>
+                <div role="list">
+                    <div role="listitem">
+                        <a hreflang="sv" href="/" aria-current="page" >Svenska</a>
+                    </div>
+                    <div role="listitem">
+                        <a hreflang="en-US" href="/en">English</a>
+                    </div>
+                </div>
+            </div>
+        </FooterCol>
+    )
+}
+
+const ContactInfoFooter = () => {
+    return (
+        <FooterCol>
+            <FooterHeading>Kontaktuppgifter</FooterHeading>
+            <FooterSubheading>Telefonnummer</FooterSubheading>
+            <FooterLinks><a href="#">070-308 95 21</a></FooterLinks>
+            <FooterSubheading>E-post</FooterSubheading>
+            <FooterLinks><a href="mailto:info@utn.se">info@utn.se</a></FooterLinks>
+            <FooterSubheading>Adress</FooterSubheading>
+            <p>
+                Polacksbacken
+                <br/>
+                Hus 73
+                <br/>
+                752 37 Uppsala
+            </p>
+        </FooterCol>
+    )
+}
+
+const OperatingHoursFooter = () => { 
+    return (
+        <FooterCol>
+            <FooterHeading>Öppettider</FooterHeading>
+            <p className={`${styles.footer_paragraph} ${styles.spaced}`}>Vardagar 9:00–17:00</p>
+            <FooterSubheading>Pub på Uthgård</FooterSubheading>
+            <p className={`${styles.footer_paragraph}`}>Torsdagar: 17:30–sent</p>
+            <FooterSubheading>Café Bocken</FooterSubheading>
+            <p className={`${styles.footer_paragraph}`}>Måndagar:
+                9:30–15:00<br/>Tisdagar:&nbsp;9:30-15:00<br/>Onsdagar:&nbsp;9:30–15:00<br/>Torsdagar:&nbsp;9:30–15:00<br/>Fredagar:&nbsp;9:30–15:00
+            </p>
+        </FooterCol>
+    )
 }
 
 const Footer = () => {
-  const [windowSize, setWindowSize] = useState([
-    window.innerWidth,
-    window.innerHeight,
-  ]);
-
-  useEffect(() => {
-    const handleWindowResize = () => {
-      setWindowSize([window.innerWidth, window.innerHeight]);
-    };
-
-    window.addEventListener("resize", handleWindowResize);
-
-    return () => {
-      window.removeEventListener("resize", handleWindowResize);
-    };
-  });
-
-  var columnWidth = 3;
-
-  if (windowSize[0] < 600) {
-    columnWidth = 12;
-  }
-
-  // To be applied to all columns and socials row
-  const styling = { color: "rgba(255,255,255,.7)", fontSize: "14px" };
-
-  return (
-    <section className={`footer`}>
-      <div className={`w_layout_vflex container`}>
-        <div className={`w_layout_vflex footer_wrapper`}>
-          <div className={`w_layout_vflex footer_logo_wrapper`}>
-            <a href="/" aria-current="page" className={`w_inline_block w_current`}>
-                <img
-                  src="https://cdn.prod.website-files.com/655e29844518537470ba5b0f/65bcf578eaa70a4540ea6e8a_utn_standard_bla%20(2).png"
-                  loading="lazy" sizes="(max-width: 479px) 100vw, 187.28692626953125px"
-                  srcSet="https://cdn.prod.website-files.com/655e29844518537470ba5b0f/65bcf578eaa70a4540ea6e8a_utn_standard_bla%20(2)-p-500.png 500w, https://cdn.prod.website-files.com/655e29844518537470ba5b0f/65bcf578eaa70a4540ea6e8a_utn_standard_bla%20(2)-p-800.png 800w, https://cdn.prod.website-files.com/655e29844518537470ba5b0f/65bcf578eaa70a4540ea6e8a_utn_standard_bla%20(2)-p-1080.png 1080w, https://cdn.prod.website-files.com/655e29844518537470ba5b0f/65bcf578eaa70a4540ea6e8a_utn_standard_bla%20(2)-p-1600.png 1600w, https://cdn.prod.website-files.com/655e29844518537470ba5b0f/65bcf578eaa70a4540ea6e8a_utn_standard_bla%20(2)-p-2000.png 2000w, https://cdn.prod.website-files.com/655e29844518537470ba5b0f/65bcf578eaa70a4540ea6e8a_utn_standard_bla%20(2).png 3227w"
-                  alt="" className={`footer_logo`}
-                />
-              </a>
-            <div className={`code_embed w_embed w_iframe`}><iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2004.5168622229132!2d17.64881977699934!3d59.84055606999524!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465fc95f989af5a9%3A0x2a2ee8953bd196b6!2sUppsala%20Teknolog-%20%26%20Naturvetark%C3%A5r!5e0!3m2!1ssv!2sse!4v1728127637716!5m2!1ssv!2sse"
-                width="280" height="280" style={{border:0}} allowFullScreen="" loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade"></iframe></div>
-          </div>
-          <div className={`w_layout_vflex footer_info_wrapper`}>
-            <div className={`w_layout_vflex footer_column`}>
-              <H3>Länkar</H3>
-              <H4>Sociala medier</H4>
-              <p className={`standalone_link`}><a href="https://facebook.com/utnkar">Facebook</a><br /><a
-                  href="https://instagram.com/utnkar">Instagram</a><br /><a
-                  href="https://linkedin.com/company/utnkar">LinkedIn</a></p>
-              <H4>Språk</H4>
-              <div className={`spacer less`}></div>
-              <div className={`w_locales_list`}>
-                <div role="list" className={`w_locales_items`}>
-                  <div role="listitem" className={`w_locales_item`}><a hreflang="sv" href="/" aria-current="page"
-                      className={`w_current`}>Svenska</a></div>
-                  <div role="listitem" className={`w_locales_item`}><a hreflang="en-US" href="/en">English</a></div>
-                </div>
-              </div>
-            </div>
-            <div className={`w_layout_vflex footer_column`}>
-              <H3>Kontaktuppgifter</H3>
-              <H4>Telefonnummer</H4>
-              <p className={`standalone_link`}><a href="#">070-308 95 21</a></p>
-              <H4>E-post</H4>
-              <p className={`standalone_link`}><a href="mailto:info@utn.se">info@utn.se</a></p>
-              <H4>Adress</H4>
-              <p className={`footer_paragraph`}>Polacksbacken<br />Hus 73<br />752 37 Uppsala</p>
-            </div>
-            <div className={`w_layout_vflex footer_column`}>
-              <H3>Öppettider</H3>
-              <p className={`footer_paragraph spaced`}>Vardagar 9:00–17:00</p>
-              <H4>Pub på Uthgård</H4>
-              <p className={`footer_paragraph`}>Torsdagar: 17:30–sent</p>
-              <H4>Café Bocken</H4>
-              <p className={`footer_paragraph`}>Måndagar:
-                9:30–15:00<br />Tisdagar:&nbsp;9:30-15:00<br />Onsdagar:&nbsp;9:30–15:00<br />Torsdagar:&nbsp;9:30–15:00<br />Fredagar:&nbsp;9:30–15:00
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <FooterWrapper>
+            <FooterLogo />
+            <FooterInfo>
+                <SocialLinksFooter />
+                <ContactInfoFooter />
+                <OperatingHoursFooter />
+            </FooterInfo>
+        </FooterWrapper>
+    );
 };
 
 export default Footer;
