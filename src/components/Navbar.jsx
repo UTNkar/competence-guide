8b0309bdb86e6f3b19f47c0e5e5028f60e99b798
Freez1t <sebastian.akerhielm@utn.se>
2024-12-09 11:40:29 +0100

    removed unused stuff

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,4 @@
-import { useEffect, useState, Fragment } from "react";
-//will be removed/changed
-
-// MUI
-import { Link, Typography, Toolbar, AppBar } from "@mui/material";
-
-// Custom components
-import { ReactComponent as UTNLogo } from "../assets/utn_logo_blue.svg";
+import { useState } from "react";
 
 import styles from "../css/FooterNavbarComponents/navbar.module.css"
 
@@ -203,102 +196,16 @@ function NavbarDropdownItem({ title, url }) {
     <main className={`${styles.w_layout_vflex} ${styles.dropdown_item_hover_target}`}>
       <a href={url} aria-current="page"
         className={`${styles.navbar_dropdown_list_item_wrapper} ${styles.w_inline_block} ${styles.navbar_dropdown_item}`}
-      // style="border-color: rgb(0,69,154);"
       >
         <div className={`${styles.w_layout_vflex} ${styles.navbar_dropdown_list_item_label_wrapper}`}>
-          <h4 className={`${styles.navbar_dropdown_list_label} ${styles.without_decoration} ${styles.without_spacing}`}>{title}</h4>
-          <h4 className={`${styles.navbar_dropdown_list_label} ${styles.arrow} ${styles.without_decoration} ${styles.without_spacing} ${styles.navbar_dropdown_item_icon}`}
-          // style="opacity: 1; transform: translate3d(4px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d; color: rgb(0,69,154); "
-          >
-            ➚</h4>
+          <h4 className={`${styles.navbar_dropdown_list_label} ${styles.without_decoration} ${styles.without_spacing}`}>
+            {title}
+          </h4>
+          <h4 className={`${styles.navbar_dropdown_list_label} ${styles.arrow} ${styles.without_decoration} ${styles.without_spacing} ${styles.navbar_dropdown_item_icon}`}>
+            ➚
+          </h4>
         </div>
       </a>
     </main>
   );
 }
-
-
-
-// import React, { useState } from "react";
-// import { AnimatePresence, motion } from "framer-motion";
-
-// const Example = () => {
-//   return (
-//     <div className="flex h-[500px] justify-center bg-neutral-900 px-3 py-12">
-//       <FlyoutLink href="#" FlyoutContent={PricingContent}>
-//         Pricing
-//       </FlyoutLink>
-//     </div>
-//   );
-// };
-
-// const FlyoutLink = ({ children, href, FlyoutContent }) => {
-//   const [open, setOpen] = useState(false);
-
-//   const showFlyout = FlyoutContent && open;
-
-//   return (
-//     <div
-//       onMouseEnter={() => setOpen(true)}
-//       onMouseLeave={() => setOpen(false)}
-//       className="relative w-fit h-fit"
-//     >
-//       <a href={href} className="relative text-white">
-//         {children}
-//         <span
-//           style={{
-//             transform: showFlyout ? "scaleX(1)" : "scaleX(0)",
-//           }}
-//           className="absolute -bottom-2 -left-2 -right-2 h-1 origin-left scale-x-0 rounded-full bg-indigo-300 transition-transform duration-300 ease-out"
-//         />
-//       </a>
-//       <AnimatePresence>
-//         {showFlyout && (
-//           <motion.div
-//             initial={{ opacity: 0, y: 15 }}
-//             animate={{ opacity: 1, y: 0 }}
-//             exit={{ opacity: 0, y: 15 }}
-//             style={{ translateX: "-50%" }}
-//             transition={{ duration: 0.3, ease: "easeOut" }}
-//             className="absolute left-1/2 top-12 bg-white text-black"
-//           >
-//             <div className="absolute -top-6 left-0 right-0 h-6 bg-transparent" />
-//             <div className="absolute left-1/2 top-0 h-4 w-4 -translate-x-1/2 -translate-y-1/2 rotate-45 bg-white" />
-//             <FlyoutContent />
-//           </motion.div>
-//         )}
-//       </AnimatePresence>
-//     </div>
-//   );
-// };
-
-// const PricingContent = () => {
-//   return (
-//     <div className="w-64 bg-white p-6 shadow-xl">
-//       <div className="mb-3 space-y-3">
-//         <h3 className="font-semibold">For Individuals</h3>
-//         <a href="#" className="block text-sm hover:underline">
-//           Introduction
-//         </a>
-//         <a href="#" className="block text-sm hover:underline">
-//           Pay as you go
-//         </a>
-//       </div>
-//       <div className="mb-6 space-y-3">
-//         <h3 className="font-semibold">For Companies</h3>
-//         <a href="#" className="block text-sm hover:underline">
-//           Startups
-//         </a>
-//         <a href="#" className="block text-sm hover:underline">
-//           SMBs
-//         </a>
-//         <a href="#" className="block text-sm hover:underline">
-//           Enterprise
-//         </a>
-//       </div>
-//       <button className="w-full rounded-lg border-2 border-neutral-950 px-4 py-2 font-semibold transition-colors hover:bg-neutral-950 hover:text-white">
-//         Contact sales
-//       </button>
-//     </div>
-//   );
-// };
